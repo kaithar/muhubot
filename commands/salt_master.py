70abd184a55bfc148f95539cf0c4bfe07e85557f
Kaithar <noreply.commiter@the-cell.co.uk>
2016-05-12 03:59:02 +0100

    Let's try that again...

--- commands/salt_master.py
+++ commands/salt_master.py
@@ -11,19 +11,45 @@ import re
 #     }
 # )
 
+def formatter_raw(result):
+    ans = []
+    for h,a in result.items():
+        ans.append("{} responded with {}".format(h,repr(a)))
+    return '\n'.join(ans)
+
+#{
+#    'git_|-git@blahblah_|-git@blahblah_|-latest': {
+#        'comment': 'Repository /blahblah/ is up-to-date', 'name': 'git@blahblah', 'start_time': '02:17:43.048571',
+#        'result': True, 'duration': 3455.455, '__run_num__': 3, 'changes': {}, '__id__': 'git@blahblah'},
+# ....
+#}
+
+def formatter_state(result):
+    ans = []
+    for h,a in result.items():
+        state_lines = []
+        for s, a2 in a.items():
+            sp = s.split('_|-')
+            a2['PF'] = 'P' if a2['result'] else 'F'
+            a2['short'] = "%s.%s"%(sp[0], sp[-1])
+            state_lines.append("{start_time} {PF} {short}: {name}: {comment} (took {duration}ms)".format(**a2))
+        ans.append("{} responded with \n{}".format(h,"\n".join(state_lines)))
+    return '\n'.join(ans)
+
 deployments = None
 
 def run(target='*', instruction='test.ping', cmd_arg=None):
+    if not cmd_arg:
+        cmd_arg = ()
     def salt_runner(*args, **kwargs):
         print "In salt_runner"
-        if not cmd_arg:
-            cmd_arg = ()
         client = LocalClient()
         t = client.cmd(target, instruction, arg=cmd_arg, timeout=120)
-        ans = []
-        for h,a in t.items():
-            ans.append("{} responded with {}".format(h,repr(a)))
-        return '\n'.join(ans)
+        if (instruction == "state.sls"):
+            ans = formatter_state(t)
+        else:
+            ans = formatter_raw(t)
+        return ans
     return salt_runner
 
 
@@ -35,10 +61,11 @@ def do_macro(registry, user, instruction, match):
     print "Starting macro %s"%macro
     client = LocalClient()
     t = client.cmd(tgt['target'], tgt['instruction'], arg=tgt['arg'], timeout=120)
-    ans = []
-    for h,a in t.items():
-        ans.append("{} responded with {}".format(h,repr(a)))
-    return {'success': True, 'answer': '\n'.join(ans)}
+    if (instruction == "state.sls"):
+        ans = formatter_state(t)
+    else:
+        ans = formatter_raw(t)
+    return {'success': True, 'answer': ans}
 
 def register(deps):
     global deployments
